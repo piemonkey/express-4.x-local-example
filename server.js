c99123597e9510d521c5c3a56ad751ef268e6556
Rich <piemonkey@gmail.com>
2015-10-08 12:25:18 +0100

    Move initialisation of passport and middleware into module

--- server.js
+++ server.js
@@ -1,46 +1,5 @@
 var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var sessions = require('client-sessions');
-var db = require('./db');
-
-
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-
+var userMiddleware = require('./middleware/users');
 
 
 // Create a new Express application.
@@ -55,17 +14,8 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(sessions({
-  cookieName: 'session',
-  secret: 'GastropubsquidkeffiyehstreetartPBRtousleddisruptroofpartytwitter',
-  duration: 3 * 24 * 60 * 60 * 1000,
-  activeDuration: 24 * 60 * 60 * 1000
-}));
+userMiddleware.setMiddleware(app);
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Define routes.
 app.get('/',
@@ -79,7 +29,7 @@ app.get('/login',
   });
 
 app.post('/login',
-  passport.authenticate('local', { failureRedirect: '/login' }),
+  userMiddleware.authenticate('local', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
